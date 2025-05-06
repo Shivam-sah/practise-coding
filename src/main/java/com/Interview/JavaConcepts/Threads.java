b80aea1b5fe0ed61590a3a7dc1f6657c96d179a5
shivam <shivam_sah@cms.co.in>
2025-05-06 17:27:47 +0530

    thread implementation

---
+++ src/main/java/com/Interview/JavaConcepts/Threads.java
@@ -0,0 +1,39 @@
+package com.Interview.JavaConcepts;
+
+
+public class Threads {
+	
+	public static class Mythread extends Thread{		
+		@Override
+        public void run() {
+            System.out.println("Extending Thread Class is running");
+        }
+		
+	}
+	 
+	
+	public static class Mythread1 implements Runnable{
+		@Override
+		public void run() {
+			System.out.println("Implementing Runnable Thread Class is running");			
+		}		
+	}
+	
+	public static void main(String[] args) {
+
+		
+		Mythread thread = new Mythread();
+		thread.start();
+		
+		Mythread1 thread1 = new Mythread1();
+		Thread thread2 = new Thread(thread1);
+		thread2.start();
+		
+		// Lambda Thread Created
+        Thread thread3 = new Thread(() -> {
+        	System.out.println("Lambda Expression Thread Class is Running Thread Class is running");
+        });
+        
+	}
+
+}
